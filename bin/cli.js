fc7ec7ad8eecdb2901897a10b8e4fb96651e5591
Saviour1001 <harshghodkar@gmail.com>
2022-11-20 19:18:08 +0530

    made a cli tool

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(command, { stdio: "inherit" });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone
+    --single-branch
+    https://github.com/Saviour1001/nextjs-fevm
+    ${repoName}`;
+
+const installCommand = `cd ${repoName} && npm install`;
+
+console.log("Cloning the repo...");
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) {
+  console.log("Error cloning the repo");
+  process.exit(1);
+}
+
+console.log("Installing dependencies...");
+const installed = runCommand(installCommand);
+if (!installed) {
+  console.log("Error installing dependencies");
+  process.exit(1);
+}
+
+console.log("Done!");
